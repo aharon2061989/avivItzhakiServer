d31f4db29556607642302156056cdb3e6e8731a1
aharon ben shimol <129784349+aharon2061989@users.noreply.github.com>
2024-07-18 09:05:59 +0300

    Add files via upload
    first push

---
+++ avivItzhakiServer/src/main/java/avivItzhaki/AvivItzhakiApplication.java
@@ -0,0 +1,13 @@
+package avivItzhaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvivItzhakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvivItzhakiApplication.class, args);
+	}
+
+}
---
+++ avivItzhakiServer/src/test/java/avivItzhaki/AvivItzhakiApplicationTests.java
@@ -0,0 +1,13 @@
+package avivItzhaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvivItzhakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
